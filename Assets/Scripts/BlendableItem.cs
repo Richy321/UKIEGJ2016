a8da95f3d7573c0d049dcbf8f6669b822512bfb8
fdavies93 <fdavies93@gmail.com>
2016-04-04 19:18:28 +0100

    Pushing blending scripts

--- Assets/Scripts/BlendableItem.cs
+++ Assets/Scripts/BlendableItem.cs
@@ -8,6 +8,8 @@ public class BlendableItem : MonoBehaviour
     public Material lightMaterial;
     public float duration = 2.0f;
     public Renderer rend;
+    private float curLerp = 0.0f;
+    private bool doLerp = false;
 
     // Use this for initialization
     void Start ()
@@ -15,11 +17,28 @@ public class BlendableItem : MonoBehaviour
         rend = GetComponent<Renderer>();
         rend.material = darkMaterial;
     }
+
+    public void StartLerp()
+    {
+        if (!doLerp && curLerp < 1.0f)
+        {
+            doLerp = true;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    float lerp = Time.deltaTime/duration;
-        rend.material.Lerp(darkMaterial, lightMaterial, lerp);
+        if (doLerp)
+        {
+            if (curLerp < 1)
+            {
+                curLerp += Time.deltaTime / duration;
+                curLerp = Mathf.Clamp01(curLerp);
+            }
+            else { doLerp = false; }
+
+            rend.material.Lerp(darkMaterial, lightMaterial, curLerp);
+        }
     }
 }
