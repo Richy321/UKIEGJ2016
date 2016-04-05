36d960881574ef3137863cde61def0ea6fe0e213
Richard Fox <rjfox321@gmail.com>
2016-04-05 10:12:33 +0100

    Freeze Mechanic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,117 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
     public LevelSection mySection;
 
+    public enum EnemyState
+    {
+        Patrol,
+        Chase,
+        Frozen
+    }
+    public EnemyState enemyState;
+    private float timeFrozen = 0.0f;
+    private float maxFrozen = 3.0f;
+    private bool isCaptureable = false;
+
+    private SphereCollider radiusCollider;
+    public MeshRenderer meshRend;
+    public List<Vector3> Waypoints;
+    public int waypointIndex;
+
+    public Transform target;
+
+        void Start()
+    {
+        radiusCollider = GetComponent<SphereCollider>();
+    }
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case EnemyState.Patrol:
+                //move towards waypoint
+                //change to next in distance
+                break;
+            case EnemyState.Chase:
+                //move towards target if not null
+                break;
+            case EnemyState.Frozen:
+                //no not fucking move you cunt
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void Freeze()
+    {
+        enemyState = EnemyState.Frozen;
+        timeFrozen = 0.0f;
+        meshRend.material.color = Color.blue;
+        Debug.Log("Freeze");
+    }
+
+    public void UnFreeze()
+    {
+        timeFrozen = 0.0f;
+        isCaptureable = false;
+        Patrol();
+        Debug.Log("UnFreeze");
+    }
+
+    public void Patrol()
+    {
+        meshRend.material.color = Color.green;
+        enemyState = EnemyState.Patrol;
+    }
+
+    public void Chase(Transform newTarget)
+    {
+        meshRend.material.color = Color.red;
+        target = newTarget;
+    }
+
+    public void Capture()
+    {
+        meshRend.material.color = Color.magenta;
+        mySection.EnemiesLeft--;
+        Die();
+    }
+
+    public void Die()
+    {
+        Destroy(this);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && enemyState != EnemyState.Frozen)
+        {
+            Freeze();
+        }
+
+        if (enemyState == EnemyState.Frozen)
+        {
+            timeFrozen += Time.deltaTime;
+
+            if (timeFrozen >= maxFrozen)
+            {
+                isCaptureable = true;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Bullet" && enemyState == EnemyState.Frozen)
+        {
+            if(other != radiusCollider)
+                UnFreeze();
+        }
+    }
 }
--- Assets/Scripts/Laz.cs
+++ Assets/Scripts/Laz.cs
@@ -11,7 +11,8 @@ public class Laz : MonoBehaviour {
 	public float growDuration = 0.5f;
 	public float growTime = 0.0f;
 
-	void Start(){
+	void Start()
+    {
 		laserLine.SetWidth (0.2f, 0.2f);
 		laserLine.enabled = false;
 	}
@@ -20,18 +21,22 @@ public class Laz : MonoBehaviour {
 	{
 		Vector3 endP =  Vector3.Lerp (startPoint.position, endPoint.position, Mathf.Clamp01(growTime / growDuration));
 
-		if (laserLine.enabled) 
-		{
+        Vector3 endPLocal = Vector3.Lerp(startPoint.localPosition, endPoint.localPosition, Mathf.Clamp01(growTime / growDuration));
 
+        if (laserLine.enabled) 
+		{
 			growTime += Time.deltaTime;
 			laserLine.SetPosition (0, startPoint.position);
 			laserLine.SetPosition (1, endP);
+
+		    lazerCollider.center = endPLocal - startPoint.localPosition;
+		    float length = (endPLocal - startPoint.localPosition).magnitude;
+            lazerCollider.size = new Vector3(lazerCollider.size.x, lazerCollider.size.y, length*2);
 		}
 	}
 
 	public void startLaz()
 	{
-	    lazerCollider.enabled = true;
         laserLine.enabled = true;
 		growTime = 0.0f;
 		particalSys.Play ();
@@ -40,10 +45,11 @@ public class Laz : MonoBehaviour {
 
 	public void stopLaz()
 	{
-	    lazerCollider.enabled = false;
+        lazerCollider.center = Vector3.zero;
+	    lazerCollider.size = new Vector3(lazerCollider.size.x, lazerCollider.size.y, 0.1f);
+
         laserLine.enabled = false;
 		growTime = 0.0f;
 		particalSys.Stop (); 
 	}
-
 }
